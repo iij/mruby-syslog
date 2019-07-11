bfe5162bff611b0b9d9a5f1f025396efd0cd6eb0
Tomoyuki Sahara <tsahara@iij.ad.jp>
2019-07-11 16:02:41 +0900

    run tests on mruby-master
     - iij/mruby-io has been incorporated to mruby.
     - we need "conf.enable_test" to run tests.

--- run_test.rb
+++ run_test.rb
@@ -20,7 +20,9 @@ MRuby::Build.new do |conf|
   toolchain :gcc
   conf.gembox 'default'
 
-  conf.gem github: 'iij/mruby-io'
+  #conf.gem github: 'iij/mruby-io'
 
   conf.gem File.expand_path(File.dirname(__FILE__))
+
+  conf.enable_test
 end
